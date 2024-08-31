9b9921fd11d264285c3bfebeb621e4a084d5181f
Jacob R. Edwards <jacob@jacobedwards.org>
2024-08-30 18:17:15 -0700

    Update the registration page
    Made it a bit prettier adding a background image for some contrast
    and styling the form a bit differently. Also added email input
    field.

--- files/register/main.js
+++ files/register/main.js
@@ -1,41 +1,42 @@
 import * as api from "/lib/api.js"
 import * as etc from "/lib/etc.js"
 
-let default_page = "/floorplans"
-
-function handle_creation(resp) {
-	window.location.href = "/login"
-}
-
-function register(username, password, err_callback) {
-	api.fetch("POST", "users", { "username": username, "password": password })
-		.then(handle_creation)
-		.catch(err_callback)
-	return false;
-}
-
 function init() {
 	if (api.authorized_duration() > 0) {
 		// Maybe don't do this?
-		window.location.href = default_page
+		window.location.href = "/floorplans"
 	}
 
+	let email_input = document.getElementById("email")
+	let email_strict_input = document.getElementById("email-strict")
 	let username_input = document.getElementById("username")
 	let password_input = document.getElementById("password")
-	if (!username_input || !password_input) {
-		throw new Error("unable to select username or password")
+	if (!email_input || !email_strict_input || !username_input || !password_input) {
+		throw new Error("Unable to select email, username and password fields")
 	}
 
 	let form = document.getElementById("register")
 	if (!form) {
-		throw new Error("unable to get register form")
+		throw new Error("Unable to select registration form")
 	}
-	form.onsubmit = function () {
-		return register(
-			username_input.value, password_input.value,
-			function (error) { return etc.error(error, form) }
-		);
-	};
+	form.addEventListener("submit", function(event) {
+		event.preventDefault()
+		api.register(username_input.value, password_input.value, email_input.value,
+		    { email_policy: email_strict_input.value })
+			.then(function() {
+				api.login(username_input.value, password_input.value)
+					.then(function() {
+						window.location.href = "/floorplans"
+					})
+					.catch(function(err) {
+						console.error("Created user but was unable to login")
+						window.location.href = "/login"
+					})
+			})
+			.catch(function (error) {
+				return etc.error(error, form)
+			})
+	})
 }
 
 window.onload = etc.handle_wrap(init)
