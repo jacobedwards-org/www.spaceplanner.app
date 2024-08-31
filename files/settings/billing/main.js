eab403a57f52a941ad4f80f266b90aed3087e449
Jacob R. Edwards <jacob@jacobedwards.org>
2024-08-30 18:24:19 -0700

    Add billing page
    This page provides access to the Stripe Billing Portal via the
    floorplan API.

---
+++ files/settings/billing/main.js
@@ -0,0 +1,19 @@
+import * as etc from "/lib/etc.js"
+import * as api from "/lib/api.js"
+
+function main() {
+        if (!api.logged_in()) {
+                window.location.href = "/login"
+        }
+
+	api.fetch("POST", "users/" + localStorage.getItem("username") + "/services/billingportal")
+		.then(function(body) {
+			window.location.href = body.url
+		})
+		.catch(function(err) {
+			console.error("Unable to display billing portal:", err)
+			etc.error("There was an unexpected error.")
+		})
+}
+
+main()
