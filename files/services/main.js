79e769c8ab6e2861219907a5e432e0a992a5b8f1
Jacob R. Edwards <jacob@jacobedwards.org>
2024-10-11 13:27:29 -0700

    Update services page
    1. Restyle it so it's not so ugly
    2. If there's only one service, just initiate checkout with it
    3. Ensure there's a verified email before checkout (this is checked
       server-side, but doing it here means I can redirect them to verify
       it)

--- files/services/main.js
+++ files/services/main.js
@@ -2,11 +2,28 @@ import * as etc from "/lib/etc.js"
 import * as api from "/lib/api.js"
 
 function main() {
-	api.fetch("GET", "services")
-		.then(function(services) {
-			console.warn(services)
-			for (let service in services) {
-				addService(services[service])
+	let wait = document.body.appendChild(document.createElement("p"))
+	wait.appendChild(document.createTextNode("Please wait..."))
+	wait.id = "wait"
+
+	etc.userService()
+		.then(function(service) {
+			if (service) {
+				window.location.href = "/billing"
+			} else {
+				api.fetch("GET", "services")
+					.then(function(services) {
+						if (services.length === 1) {
+							chooseService(services[0])
+						} else {
+							for (let service in services) {
+								addService(services[service])
+							}
+							wait.remove()
+							document.getElementById("services")
+								.removeAttribute("hidden", false)
+						}
+					})
 			}
 		})
 }
@@ -18,15 +35,21 @@ function chooseService(service) {
                 window.location.href = "/register"
         }
 
-        api.fetch("POST", "users/:user/services/checkout",
-                { prices: [ service.prices[0].id ] }).then(function(body) {
-			console.warn(body)
-                        window.location.href = body.url
-                })
-                .catch(function(err) {
-                        console.error("Unable to checkout:", err)
-                        etc.error("There was an unexpected error.")
-                })
+	api.verifiedEmail()
+		.then(function(email) {
+			if (!email) {
+				window.location.href = "/settings/verify-email"
+				return
+			}
+			api.fetch("POST", "users/:user/services/checkout",
+				{ prices: [ service.prices[0].id ] }).then(function(body) {
+					window.location.href = body.url
+				})
+				.catch(function(err) {
+					console.error("Unable to checkout:", err)
+					etc.error("There was an unexpected error.")
+				})
+		})
 }
 
 function addService(service) {
